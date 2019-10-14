c508a2484bd2b2813e01dd6a363e09a0bab33507
Glitch (zgleman-metric) <none>
2019-10-14 08:32:16 +0000

    🌄🍥 Checkpoint ./server.js:7037687/150

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
-
+var helmet = require('helmet');
+app.use(helmet());
+app.use(helmet.xxsFilter());
+app.use(helmet.noSniff());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
