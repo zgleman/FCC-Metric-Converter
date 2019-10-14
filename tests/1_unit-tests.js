fb56aaa5418279ee9faf70679b7cc024626b5060
Glitch (zgleman-metric) <none>
2019-10-14 12:10:48 +0000

    👹😽 Checkpoint ./tests/1_unit-tests.js:7037687/102

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -109,37 +109,37 @@ suite('Unit Tests', function(){
     
     test('L to Gal', function(done) {
       var input = [5, 'l'];
-      var expected = 18.9271;
+      var expected = 1.32086;
       assert.approximately(convertHandler.convert(input[0],input[1]),expected,0.1);
-      //done();
+      done();
     });
     
     test('Mi to Km', function(done) {
       var input = [5, 'mi'];
-      var expected = 18.9271;
+      var expected = 8.0467;
       assert.approximately(convertHandler.convert(input[0],input[1]),expected,0.1);
-      //done();
+      done();
     });
     
     test('Km to Mi', function(done) {
       var input = [5, 'km'];
-      var expected = 18.9271;
+      var expected = 3.10686;
       assert.approximately(convertHandler.convert(input[0],input[1]),expected,0.1);
-      //done();
+      done();
     });
     
     test('Lbs to Kg', function(done) {
       var input = [5, 'lbs'];
-      var expected = 18.9271;
+      var expected = 2.26796;
       assert.approximately(convertHandler.convert(input[0],input[1]),expected,0.1);
-      //done();
+      done();
     });
     
     test('Kg to Lbs', function(done) {
       var input = [5, 'kg'];
-      var expected = 18.9271;
+      var expected = 11.02312;
       assert.approximately(convertHandler.convert(input[0],input[1]),expected,0.1);
-      //done();
+      done();
     });
     
   });
